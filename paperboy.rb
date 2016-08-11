20a0951d7c516b260370d7f28f08abc475eecf4d
Thomas Spencer <t.spencer1990@hotmail.com>
2016-08-11 14:58:02 -0400

    Added payment, deliver, and report methods

--- paperboy.rb
+++ paperboy.rb
@@ -1,15 +1,57 @@
 class Paperboy
 
-  def initialize(name, expereince, side_of_street, earnings)
+  def initialize(name, side_of_street)
     @name           = name
-    @expereince     = expereince
+    @expereince     = 0
     @side_of_street = side_of_street
-    @earnings       = earnings
+    @earnings       = 0.0
   end
 
   def quota
     total = 50 + (@expereince / 2)
     return total
+  end
+
+  def payment(papers_delivered)
+    if papers_delivered > self.quota
+      overtime = papers_delivered - self.quota
+      @earnings = (overtime * 0.5) + (self.quota * 0.25)
+    elsif papers_delivered == self.quota
+      @earnings = papers_delivered * 0.25
+    else
+      @earnings = (papers_delivered * 0.25) - 2.0
+    end
+  end
+
+  def deliver(start_address, end_address)
+
+    if @side_of_street == "even"
+      total_deliveries = 0
+      (start_address..end_address).each do |x|
+        if x.even?
+          total_deliveries +=1
+        end
+      end
+      total_earnings = self.payment(total_deliveries)
+      @expereince += total_deliveries
+
+    elsif @side_of_street == "odd"
+      total_deliveries = 0
+      (start_address..end_address).each do |x|
+        if x.odd?
+          total_deliveries +=1
+        end
+      end
+      total_earnings = self.payment(total_deliveries)
+      @expereince += total_deliveries
+
+    else
+      puts "#{@name} doesn't know which side of the street to deliver to!"
+    end
+  end
+
+  def report
+    return "I'm #{@name}, I've delivered #{@expereince} papers and have earned #{@earnings} so far!"
   end
 
 end

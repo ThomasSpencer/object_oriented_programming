f8e6d4f926085be8851c82b56cf0e43a4267b74b
Thomas Spencer <t.spencer1990@hotmail.com>
2016-08-11 13:36:26 -0400

    Added the eats_at instance method

--- cat.rb
+++ cat.rb
@@ -1,9 +1,18 @@
-class Cat(name, preferred_food, meal_time)
+class Cat
 
   def initialize(name, preferred_food, meal_time)
     @name = name
     @preferred_food = preferred_food
     @meal_time = meal_time
+  end
+
+  def eats_at
+    if @meal_time > 12
+      evening = @meal_time - 12
+      return "#{evening} PM"
+    else
+      return "#{@meal_time} AM"
+    end
   end
 
 end

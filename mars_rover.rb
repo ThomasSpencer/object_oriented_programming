35a264eea5a697208fd0f92378d1376b2db6e961
Thomas Spencer <t.spencer1990@hotmail.com>
2016-08-11 15:45:52 -0400

    Added move and turn methods

---
+++ mars_rover.rb
@@ -0,0 +1,52 @@
+class Rover
+
+  def initialize(x, y, direction)
+    @x = x
+    @y = y
+    @direction = direction
+  end
+
+  def read_instruction()
+    
+  end
+
+  def move
+    if @direction == "N"
+      @y += 1
+    elsif @direction == "E"
+      @x += 1
+    elsif @direction == "S"
+      @y -= 1
+    elsif @direction == "W"
+      @x -= 1
+    else
+      puts "The rover does not have a valid direction option"
+    end
+  end
+
+  def turn(rotate)
+    case rotate
+    when "L"
+      if @direction == "N"
+        @direction = "W"
+      elsif @direction == "W"
+        @direction = "S"
+      elsif @direction == "S"
+        @direction = "E"
+      elsif @direction == "E"
+        @direction = "N"
+      end
+    when "R"
+      if @direction == "N"
+        @direction = "E"
+      elsif @direction == "W"
+        @direction = "N"
+      elsif @direction == "S"
+        @direction = "W"
+      elsif @direction == "E"
+        @direction = "S"
+      end
+    end
+  end
+
+end
